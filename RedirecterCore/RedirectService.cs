22937814283857a2aa384984b60984a9bb08b945
Sven Schuermann <schuermann-sven@t-online.de>
2023-11-04 10:34:52 +0100

    check the validity of notvalidbefore and validuntil

--- RedirecterCore/RedirectService.cs
+++ RedirecterCore/RedirectService.cs
@@ -9,21 +9,54 @@ namespace Redirecter
 
         internal IRedirectStorageProvider serviceProvider;
 
+        /// <inheritdoc/>
         internal RedirectService(IRedirectStorageProvider serviceProvider)
         {
             this.serviceProvider = serviceProvider;
         }
 
+        /// <inheritdoc/>
         public RedirectModel? GetById(Guid id)
         {
-            return serviceProvider.GetById(id);
+            var model = serviceProvider.GetById(id);
+
+            if (model is not null && CheckValidity(model) is 0) model = null;
+
+            return model;
         }
 
+        /// <inheritdoc/>
         public RedirectModel? GetByName(string name)
         {
-            return serviceProvider.GetByName(name.ToLower());
+            var model = serviceProvider.GetByName(name.ToLower());
+
+            if (model is not null && CheckValidity(model) is 0) model = null;
+
+            return model;
         }
 
+        /// <summary>
+        /// Check if the validity of Date and time are valid
+        /// </summary>
+        /// <param name="model">the redirect model</param>
+        /// <returns>0 if valid; -1 if not valid before; 1 if not valid until</returns>
+        private int CheckValidity(RedirectModel model)
+        {
+            // Check if valid before is in the past
+            if (model.NotValidBefore > DateTimeOffset.UtcNow)
+            {
+                return -1;
+            }
+
+            // Check if valid until is in the future
+            if (model.ValidUntil < DateTimeOffset.UtcNow)
+            {
+                return 1;
+            }
+
+
+            return 0;
+        }
          
     }
 }
